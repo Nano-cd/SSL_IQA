cf054637bf05fce13f5630163e0e6c4e2cbe8e78
Di Cheng <72659127+Nano-cd@users.noreply.github.com>
2023-04-21 16:54:23 +0800

    Add files via upload

---
+++ dataset/train_dataset.py
@@ -0,0 +1,36 @@
+import torch
+from torch.utils.data import Dataset
+import os
+from PIL import Image
+import torchvision
+
+data_transform_train = torchvision.transforms.Compose([
+    torchvision.transforms.Resize([512, 384]),
+    torchvision.transforms.RandomCrop(size=224),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+])
+
+
+class train_data(Dataset):
+    def __init__(self, label_file, image_root, patch_num):
+        self.imagepaths = []
+        self.labels = []
+        self.sample = []
+        self.transforms = data_transform_train
+
+        with open(label_file, 'r') as f:
+            for line in f.readlines():  # 读取label文件
+                for aug in range(patch_num):
+                    self.imagepaths.append(os.path.join(image_root, line.split()[5]))
+                    self.labels.append([float(line.split()[0]),float(line.split()[1]),float(line.split()[2])
+                                    ,float(line.split()[3]),float(line.split()[4])])
+
+    def __getitem__(self, item):
+        x = Image.open(self.imagepaths[item])
+        y = torch.tensor(self.labels[item], dtype=torch.float)
+        return self.transforms(x), y
+
+    def __len__(self):
+        return len(self.imagepaths)
